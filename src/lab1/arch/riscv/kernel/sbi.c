9a078a6c5bf96420cfba9a0acbfeb3af3faac454
Starrism <hyw-luv@zju.edu.cn>
2024-10-15 05:11:23 +0000

    [submit] Lab1

--- src/lab1/arch/riscv/kernel/sbi.c
+++ src/lab1/arch/riscv/kernel/sbi.c
@@ -4,13 +4,55 @@
 struct sbiret sbi_ecall(uint64_t eid, uint64_t fid,
                         uint64_t arg0, uint64_t arg1, uint64_t arg2,
                         uint64_t arg3, uint64_t arg4, uint64_t arg5) {
-    #error Unimplemented
+
+    struct sbiret ret;
+    asm volatile(
+        "mv a7, %[eid]\n"
+        "mv a6, %[fid]\n"
+        "mv a0, %[arg0]\n"
+        "mv a1, %[arg1]\n"
+        "mv a2, %[arg2]\n"
+        "mv a3, %[arg3]\n"
+        "mv a4, %[arg4]\n"
+        "mv a5, %[arg5]\n"
+        "ecall\n"
+        "mv %[error], a0\n"
+        "mv %[value], a1\n"
+        : [error] "=r"(ret.error), [value] "=r"(ret.value)
+        : [eid] "r"(eid), [fid] "r"(fid), [arg0] "r"(arg0), [arg1] "r"(arg1), [arg2] "r"(arg2), [arg3] "r"(arg3), [arg4] "r"(arg4), [arg5] "r"(arg5)
+        : "memory", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7"
+    );
+    return ret;
 }
 
 struct sbiret sbi_debug_console_write_byte(uint8_t byte) {
-    #error Unimplemented
+    struct sbiret ret;
+    asm volatile(
+        "mv a7, %[eid]\n"
+        "mv a6, %[fid]\n"
+        "mv a0, %[byte]\n"
+        "ecall\n"
+        "mv %[error], a0\n"
+        "mv %[value], a1\n"
+        : [error] "=r"(ret.error), [value] "=r"(ret.value)
+        : [eid] "r"(0x01), [fid] "r"(0), [byte] "r"(byte)
+        : "memory", "a0", "a1", "a6", "a7"
+    );
 }
 
 struct sbiret sbi_system_reset(uint32_t reset_type, uint32_t reset_reason) {
-    #error Unimplemented
+    struct sbiret ret;
+    asm volatile(
+        "mv a7, %[eid]\n"
+        "mv a6, %[fid]\n"
+        "mv a0, %[reset_type]\n"
+        "mv a1, %[reset_reason]\n"
+        "ecall\n"
+        "mv %[error], a0\n"
+        "mv %[value], a1\n"
+        : [error] "=r"(ret.error), [value] "=r"(ret.value)
+        : [eid] "r"(0x08), [fid] "r"(0), [reset_type] "r"(reset_type), [reset_reason] "r"(reset_reason)
+        : "memory", "a0", "a1", "a6", "a7"
+    );
+    return ret;
 }
--- src/lab1/init/test.c
+++ src/lab1/init/test.c
@@ -1,6 +1,22 @@
 #include "sbi.h"
+#include "printk.h"
 
-void test() {
-    sbi_system_reset(SBI_SRST_RESET_TYPE_SHUTDOWN, SBI_SRST_RESET_REASON_NONE);
-    __builtin_unreachable();
+// test for kernel_boot
+// void test() {
+//     sbi_system_reset(SBI_SRST_RESET_TYPE_SHUTDOWN, SBI_SRST_RESET_REASON_NONE);
+//     __builtin_unreachable();
+// }
+
+// test for interrupt
+void test()
+{
+    int i = 0;
+    while (1)
+    {
+        if ((++i) % 100000000 == 0)
+        {
+            printk("kernel is running!\n");
+            i = 0;
+        }
+    }
 }
